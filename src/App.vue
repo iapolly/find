35a99ba7356ff7dd375aac10faf6a5a376a9a7e5
Alikina Polina <polina7alikina@gmail.com>
2018-11-05 17:34:20 +0300

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img src="static/img/logo.png" v-draggable>
-  </div>
+  <!--<div id="app">-->
+    <!--<img src="static/img/logo.png" v-draggable>-->
+  <!--</div>-->
+  <div class="app"></div>
 </template>
 
 <script>
-import { Draggable } from 'draggable-vue-directive'
+/* import { Draggable } from 'draggable-vue-directive' */
 
 export default {
-  name: 'App',
-  directives: {
-    Draggable
-  }
+  name: 'App'
+  // directives: {
+  //   Draggable
+  // }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '/components/HelloWorld'
+
+var Menu = require('./components/Manu.vue')
+var Rules = require('./components/Rules.vue')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/menu', component: Menu},
+    {path: 'rules', component: Rules}
   ]
 })
