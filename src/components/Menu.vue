9d2765a98dd7134469036d5729008bd0130de3c5
Alikina Polina <polina7alikina@gmail.com>
2018-11-05 17:35:09 +0300

    add menu and rules vue components

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header text-center">
+    <h1>Welcome to the Redbook game!</h1>
+    <br>
+    <br>
+    <ul>
+      <a href="#" id="start"><li><h3>START</h3></li></a>
+      <li><router-link to="/rules"><div class="rules"><h3>RULES</h3></div></router-link></li>
+      <a href="credits.html" id="credits"><li><h3>CREDITS</h3></li></a>
+      <a href="settings.html" id="settings"><li><h3>SETTINGS</h3></li></a>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style src="static/css/style.css"></style>
---
+++ src/components/Rules.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header text-center">
+    <h1>RULES:</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Rules'
+}
+</script>
+
+<style src="static/css/style.css"></style>
