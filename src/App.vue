b11152669323a8af2c510ce6c1d4eabdf1536fc3
Alikina Polina <polina7alikina@gmail.com>
2018-11-05 13:57:06 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img src="static/img/logo.png" v-draggable>
+  </div>
+</template>
+
+<script>
+import { Draggable } from 'draggable-vue-directive'
+
+export default {
+  name: 'App',
+  directives: {
+    Draggable
+  }
+}
+</script>
+
+<style>
+
+</style>
