a4e29155b95129611d04fdf23425c90468de8c1e
Alikina Polina <polina7alikina@gmail.com>
2018-11-06 15:41:36 +0300

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-var Menu = require('./components/Manu.vue')
-var Rules = require('./components/Rules.vue')
+import Menu from '../components/Menu'
+import Rules from '../components/Rules'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/menu', component: Menu},
-    {path: 'rules', component: Rules}
+    {path: '/rules', component: Rules}
   ]
 })
