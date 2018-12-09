310d727643d64231f3780bfc888e9669a9ec4f84
Alikina Polina <polina7alikina@gmail.com>
2018-12-09 14:57:28 +0300

    add player

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="table">
       <ball></ball>
+      <player></player>
     </div>
 </template>
 
 <script>
 import ball from '../components/table-objects/Ball'
+import player from '../components/table-objects/Player'
 
 export default {
   name: 'table',
   components: {
-    ball
+    ball,
+    player
   }
 }
 </script>
---
+++ src/components/table-objects/Player.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="table-object">
+    <img id="player" v-draggable v-on:dblclick="play" src="/static/img/плеер.png">
+    <audio ref="audioElm" src="/static/mp3/kol.mp3"></audio>
+  </div>
+</template>
+
+<script>
+import { Draggable } from 'draggable-vue-directive'
+
+export default {
+  name: 'player',
+  data: function () {
+    return {
+      playFlag: false
+    }
+  },
+  methods: {
+    play: function (e) {
+      let audio = this.$refs.audioElm;
+      if (!this.$data.playFlag) {
+        audio.play();
+        this.$data.playFlag = true;
+      } else {
+        audio.pause();
+        this.$data.playFlag = false;
+      }
+    }
+  },
+  directives: {
+    Draggable
+  }
+}
+</script>
+
+<style scoped>
+  #player {
+    width: 100px;
+    height: 100px;
+  }
+</style>
