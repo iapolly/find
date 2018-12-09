1a78ac77fd8994ab7bb2d218de596e4f1eb11217
Alikina Polina <polina7alikina@gmail.com>
2018-12-09 20:33:49 +0300

    add lipstick

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,18 +2,24 @@
     <div id="table">
       <ball></ball>
       <player></player>
+      <laptop></laptop>
+      <lipstick></lipstick>
     </div>
 </template>
 
 <script>
 import ball from '../components/table-objects/Ball'
 import player from '../components/table-objects/Player'
+import  laptop from './table-objects/Laptop'
+import lipstick from './table-objects/Lipstick'
 
 export default {
   name: 'table',
   components: {
     ball,
-    player
+    player,
+    laptop,
+    lipstick
   }
 }
 </script>
---
+++ src/components/table-objects/Lipstick.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="table-object">
+    <img id="lipstick" v-draggable v-on:click="action" src="/static/img/Pomada.png">
+    <img id="kiss" src="/static/img/kiss.png" hidden>
+  </div>
+</template>
+
+<script>
+  import { Draggable } from 'draggable-vue-directive'
+
+  export default {
+    name: 'lipstick',
+    methods: {
+      action: function (e) {
+        //у меня какая-то фигня
+        let kiss = document.getElementById("kiss");
+        kiss.style.marginLeft = document.getElementById("lipstick").offsetWidth + 'px';
+        kiss.style.marginTop = document.getElementById("lipstick").offsetTop + 'px';
+        kiss.hidden = !kiss.hidden;
+        setTimeout(function (){kiss.hidden = !kiss.hidden}, 1000);
+      }
+    },
+    directives: {
+      Draggable
+    }
+
+  }
+</script>
+
+<style scoped>
+  #lipstick {
+    width: 100px;
+  }
+  #kiss {
+    width: 100px;
+  }
+
+</style>
