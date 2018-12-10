d21f0ca212e4d616a4114123c54f9bc855a714de
Alikina Polina <polina7alikina@gmail.com>
2018-12-10 09:37:53 +0300

    remove style

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,11 @@
 
 <script>
 import { Draggable } from 'draggable-vue-directive'
-import WebFontLoader from 'webfontloader'
 
 export default {
   name: 'App',
   directives: {
     Draggable
-  },
-  mounted () {
-    WebFontLoader.load({
-      google: {
-        families: ['Roboto:100,300,400,500,700,900']
-      },
-      active: this.setFontLoaded
-    })
-  },
-  methods: {
-    setFontLoaded () {
-      this.$emit('font-loaded')
-    }
   }
 }
 </script>
