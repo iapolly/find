d27232439fc72c071a7a84bef26a34be25ed1ab4
Mary <bmacha23@gmail.com>
2018-12-23 17:00:32 +0300

    Merge branch 'master' of /Users/bmacha/Documents/GitHub/find with conflicts.

--- src/components/table-objects/Ball.vue
+++ src/components/table-objects/Ball.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="table-object">
     <img id="ball" v-on:dblclick="action" src="/static/img/мячик.png">
+    <img src="/static/img/bum.png" id="bum">
   </div>
 </template>
 
@@ -11,7 +12,16 @@ export default {
   name: 'ball',
   methods: {
     action: function (e) {
-      alert('работает')
+      var ball = document.getElementById("ball");
+      var bum = document.getElementById("bum");
+      bum.style.marginLeft = ball.offsetWidth -400 + 'px';
+      bum.style.marginTop = ball.offsetTop  + 'px';
+      ball.style.display = 'none';
+      bum.style.display = 'block';
+      setTimeout(function () {
+        window.location.href = 'http://localhost:8080/#/second';
+      }, 1000)
+
     }
   },
   directives: {
@@ -24,5 +34,9 @@ export default {
   /*надо либо изменить размеры всех картинок, для всех один шаблон*/
   #ball {
     width: 100px;
+  }
+  #bum {
+    display: none;
+    width: 800px;
   }
 </style>
---
+++ src/components/table-objects/Money.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="table-object">
+    <img src="/static/img/монетка.png" class="money" v-on:dblclick="del" >
+  </div>
+
+</template>
+
+<script>
+  import { Draggable } from 'draggable-vue-directive'
+
+  export default {
+    name: 'money',
+    directives: {
+      Draggable
+    },
+    methods:{
+      del: function (e) {
+
+        var money = document.getElementsByClassName("money");
+        for (let i = money.length-1; i>0; i--){
+            console.log(money[i].className);
+            money[i].parentElement.remove();
+
+
+        }
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .money{
+    width: 40px;
+    transition: all 1s ease-out 1s;
+  }
+.money:hover{
+  -moz-transform: rotate(80deg); /* Для Firefox */
+  -ms-transform: rotate(80deg); /* Для IE */
+  -webkit-transform: rotate(80deg); /* Для Safari, Chrome, iOS */
+  -o-transform: rotate(80deg); /* Для Opera */
+  transform: rotate(80deg);
+  transition: all 1s ease-out ;
+}
+</style>
