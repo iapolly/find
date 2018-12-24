ed06ac3b5885c2b24fb75f6ac38bd99786b4df60
iapolly <ris0070082>
2018-12-24 11:28:41 +0300

    big change - 3

---
+++ src/components/Music.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <img id="player" v-on:dblclick="play" src="https://img.icons8.com/ios/50/000000/speaker.png">
+      <audio ref="audioElm" src="/static/mp3/piano.mp3"></audio>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "music",
+      methods: {
+        play: function (e) {
+          let audio = this.$refs.audioElm;
+          if (!this.$data.playFlag) {
+            audio.play();
+            this.$data.playFlag = true;
+          } else {
+            audio.pause();
+            this.$data.playFlag = false;
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  #player {
+    position: fixed;
+  }
+
+</style>
---
+++ src/components/table-objects/Kiss.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="table-object" >
+    <img id="kiss" src="/static/img/kiss.png" >
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Kiss"
+    }
+</script>
+
+<style scoped>
+  #kiss {
+    width: 0;
+    position: absolute;
+    z-index: 5;
+  }
+</style>
